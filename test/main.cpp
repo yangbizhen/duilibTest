65e939972eee7db2d5c90d1d06e80efb5213bb22
yangbz <837530554@qq.com>
2017-11-10 17:52:37 +0800

    Signed-off-by: yangbz <837530554@qq.com>

---
+++ test/main.cpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "..\DuiLib\UIlib.h"
+using namespace DuiLib;
+
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual CDuiString GetSkinFile()                { return _T("ui.xml"); }
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();
+	duiFrame.ShowModal();
+	
+	return 0;
+}
